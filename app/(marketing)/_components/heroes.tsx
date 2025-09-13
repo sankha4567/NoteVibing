4577bb01d3f1d1bf7109cf43a3911b9a9e4f75d8
sankha4567 <ssankha103@gmail.com>
2025-09-13 08:29:02 +0530

    updated code

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -4,7 +4,7 @@ export const Heroes = () => {
   return (
     <div className="flex flex-col items-center justify-center max-w-5xl">
       <div className="flex items-center">
-        <div className="relative w-[400px] h-[400px] sm:w-[350px] sm:h-[350] md:h-[400px md:w-[400px]">
+        <div className="relative w-[400px] h-[400px] sm:w-[350px] sm:h-[350] md:h-[400px] md:w-[400px]">
           <Image
             src="/doc.png"
             fill
@@ -12,7 +12,7 @@ export const Heroes = () => {
             alt="Documents"
           />
           <Image
-            src="/docWhite.png"
+            src="/docwhite.png"
             fill
             className="object-contain hidden dark:block"
             alt="Documents"
@@ -20,7 +20,7 @@ export const Heroes = () => {
         </div>
         <div className="relative h-[400px] w-[400px] hidden md:block">
           <Image
-            src="/bookWhite.png"
+            src="/bookwhite.png"
             fill
             className="object-contain dark:hidden"
             alt="Reading"
